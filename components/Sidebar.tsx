d049071cd112b02cf950133cc7894e3b9bab3ef7
bju9119 <abcd@email.com>
2024-05-24 17:49:57 +0530

    modified:   components/Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ import React from 'react';
 const Sidebar = () => {
   const pathname = usePathname(); 
   return (
-    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-transparent dark:bg-black p-6 pt-28 text-lime-900 text-opacity-90 font-sans dark:text-white max-sm:hidden lg:w-[320px]">
+    <section className="sticky left-0 flex h-screen w-fit flex-col justify-between bg-transparent dark:bg-black p-6 pt-20 text-lime-900 text-opacity-90 font-sans dark:text-white max-sm:hidden lg:w-[320px]">
       <div className='flex flex-1 flex-col mt-20 gap-20'>
         {sidebarLinks.map((link) =>{
           const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
@@ -20,7 +20,7 @@ const Sidebar = () => {
             <Link
               href={link.route}
               key={link.label}
-              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+              className={cn('flex gap-2 items-center p-4 rounded-lg justify-start', {
                 'bg-green-200 dark:bg-slate-700 bg-opacity-80 dark:bg-opacity-80': isActive,
               })}
             >
