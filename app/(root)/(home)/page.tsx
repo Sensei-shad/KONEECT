b8aa2294a532148813892a2675001337e8578dbb
Sensei-shad <chakrabortysounak07@gmail.com>
2024-05-30 23:01:20 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,15 +3,13 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
   const now = new Date();
 
-
- const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
+  const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(now);
 
   return (
     <section className="flex size-full flex-col mt-8 gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5
-        max-md:py-8 lg:p-11">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl uppercase">
               {time}
@@ -23,11 +21,9 @@ const Home = () => {
         </div>
       </div>
 
+      <MeetingTypeList />
+    </section>
+  );
+};
 
-      <MeetingTypeList/>
-
-      </section>
-  )
-}
-
-export default Home
+export default Home;
