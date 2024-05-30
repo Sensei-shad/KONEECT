88a9b3b46ece88984d8a57cb134b6ac3074ca1e8
Sensei-shad <chakrabortysounak07@gmail.com>
2024-05-30 22:32:35 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,8 +4,8 @@ const Home = () => {
   const now = new Date();
 
 
-  const time = now.toLocaleTimeString('en-IN', { hour:'2-digit', minute: '2-digit'});
-  const date = (new Intl.DateTimeFormat('en-IN', {dateStyle: 'full'})).format(now);
+  const time = now.toLocaleTimeString('en-US', { hour:'2-digit', minute: '2-digit'});
+  const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
 
   return (
     <section className="flex size-full flex-col mt-8 gap-10 text-white">
