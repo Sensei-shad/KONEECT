7d134c5133865451062802c8574d2ad1085c6dc9
bju9119 <abcd@email.com>
2024-05-30 21:24:57 +0530

    modified:   app/(root)/(home)/page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,8 +4,8 @@ const Home = () => {
   const now = new Date();
 
 
-  const time = now.toLocaleTimeString('en-US', { hour:'2-digit', minute: '2-digit'});
-  const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
+  const time = now.toLocaleTimeString('en-IN', { hour:'2-digit', minute: '2-digit'});
+  const date = (new Intl.DateTimeFormat('en-IN', {dateStyle: 'full'})).format(now);
 
   return (
     <section className="flex size-full flex-col mt-8 gap-10 text-white">
@@ -13,7 +13,7 @@ const Home = () => {
         <div className="flex h-full flex-col justify-between max-md:px-5
         max-md:py-8 lg:p-11">
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">
+            <h1 className="text-4xl font-extrabold lg:text-7xl uppercase">
               {time}
             </h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
